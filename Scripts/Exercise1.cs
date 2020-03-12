4e885acbe62214c1c2e5274791daba0a4f0dd68c
Falco <f.ijben@recognize.nl>
2020-03-12 10:18:17 +0100

    first commit

---
+++ Scripts/Exercise1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine; 
+
+namespace Lab6.Scripts
+{
+    public class Exercise1 : MonoBehaviour
+    {
+        [SerializeField] private ComputeShader computeShader;
+        [SerializeField] private RenderTexture renderTexture;
+
+        private void Start()
+        {
+            renderTexture.enableRandomWrite = true;
+            renderTexture.Create();
+            var kernelIndex = computeShader.FindKernel("CSMain");
+            computeShader.SetTexture(kernelIndex, "Result", renderTexture);
+            computeShader.Dispatch(kernelIndex, renderTexture.width / 8, renderTexture.height / 8, 1);
+        }
+
+        private void OnDestroy()
+        {
+            renderTexture.Release();
+        }
+    }
+}
---
+++ Scripts/Exercise2.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Lab6.Scripts
+{
+    public class Exercise2 : MonoBehaviour
+    {
+	int iterations;
+        int bailout = 2;
+        int bottom = -1;
+        float left = -2.5f;
+        float right = 1;
+        int top = 1;
+	[SerializeField] private ComputeShader computeShader;
+        [SerializeField] private RenderTexture renderTexture;
+        [SerializeField] private Texture2D texture;
+
+        private void Start()
+        {
+            renderTexture.enableRandomWrite = true;
+            renderTexture.Create();
+            
+            var kernelIndex = computeShader.FindKernel("CSMain");
+            
+	    iterations = 100;
+            bailout = 2;
+            bottom = -1;
+            left = -2.5f;
+            right = 1;
+            top = 1; 
+            
+            computeShader.SetTexture(kernelIndex, "_ColorGradient", texture);
+            computeShader.SetTexture(kernelIndex, "_MandelbrotSet", renderTexture);
+            computeShader.Dispatch(kernelIndex, renderTexture.width / 8, renderTexture.height / 8, 1);
+        }
+
+        private void OnDestroy()
+        {
+            renderTexture.Release();
+        }
+
+        private void Reset()
+        {
+            iterations = 100;
+            bailout = 2;
+            bottom = -1;
+            left = -2.5f;
+            right = 1;
+            top = 1;
+            computeShader = null;
+            renderTexture = null;
+            texture = null;
+        }
+    }
+}
